74e8c2017d5fb35b953cf29fde9d6c86098c1bc8
Rahul Kumar <rahulkr009@gmail.com>
2021-12-25 01:47:53 +0530

    Delete react-testcase-jest directory

--- react-testcase-jest/__mocks__/math.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  add: jest.fn().mockReturnValue("a"),
-  subtract: jest.fn().mockReturnValue("s"),
-  multiply: jest.fn().mockReturnValue("m"),
-  divide: jest.fn().mockReturnValue("d")
-};
